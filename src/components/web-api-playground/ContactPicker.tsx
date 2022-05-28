847a46d8d5ba1dbe5dfdf5166d97b85164ad1454
Kevin Zeillmann <kzeillmann@gmail.com>
2022-05-28 11:34:07 -0400

    Try contact picker

--- src/components/web-api-playground/ContactPicker.tsx
+++ src/components/web-api-playground/ContactPicker.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "preact/hooks";
+
 const ContactPicker = () => {
+  const [supportMessage, setSupportMessage] = useState(
+    "❓ Undetermined if Contact Picker API is supported in your browser"
+  );
+
+  useEffect(() => {
+    const supported = "contacts" in navigator;
+    if (supported) {
+      setSupportMessage("Contact Picker API is supported in your browser");
+    } else {
+      setSupportMessage(
+        "❌Contact Picker API is not supported in your browser"
+      );
+    }
+  });
   return (
     <details id="contact-picker" open>
       <summary>Contact Picker</summary>
+      <p>
+        This is an experimental API only currently supported on Android devices.
+      </p>
+      <p>Support: {supportMessage}</p>
     </details>
   );
 };
