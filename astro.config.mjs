061c804c9bf8e10c0ea0d92eb8692f59e626016f
Kevin Zeillmann <kzeillmann@gmail.com>
2022-07-17 15:26:50 -0400

    Fight tailwind on styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solid()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    solid(),
+  ],
 });
