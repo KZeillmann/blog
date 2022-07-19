78682c9358d2414c006d6b01b2f43607723a734e
Kevin Zeillmann <kzeillmann@gmail.com>
2022-07-18 22:38:36 -0400

    Draw more to the screen

--- src/components/board-game/spirit-island/hooks/useBoard.ts
+++ src/components/board-game/spirit-island/hooks/useBoard.ts
@@ -16,15 +16,75 @@ const useBoard = () => {
   const graphRef = useRef(null);
 
   const drawLand = (ctx: CanvasRenderingContext2D, land: Land): void => {
-    ctx.fillStyle = LAND_COLOR_MAPPING[land.terrain];
     const { xStart, xEnd, yStart, yEnd } = land.coordinates;
     const width = ((xEnd - xStart) * graphRef.current.width) / 100;
     const height = ((yEnd - yStart) * graphRef.current.height) / 100;
+    // poor man's border- fill up whole square
+    ctx.fillStyle = "#000";
     ctx.fillRect(
       (xStart / 100) * graphRef.current.width,
       (yStart / 100) * graphRef.current.height,
-      width,
-      height
+      width - 2,
+      height - 2
+    );
+    // Draw land
+    ctx.fillStyle = LAND_COLOR_MAPPING[land.terrain];
+
+    ctx.fillRect(
+      (xStart / 100) * graphRef.current.width + 1,
+      (yStart / 100) * graphRef.current.height + 1,
+      width - 2,
+      height - 2
+    );
+    // Draw text on top of land
+    const fontSizePixels = 16;
+    const fontFamily = "sans-serif";
+    const textStartX = (xStart / 100) * graphRef.current.width + 10;
+    const textStartY = (yStart / 100) * graphRef.current.height;
+    ctx.font = `${fontSizePixels}px ${fontFamily}`;
+    ctx.fillStyle = "#000";
+    ctx.fillText(
+      `${land.terrain} ${land.id}`,
+      textStartX,
+      +fontSizePixels,
+      width - 2
+    );
+    ctx.fillText(
+      `Invaders:`,
+      textStartX,
+      textStartY + fontSizePixels * 2,
+      width - 2
+    );
+    ctx.fillText(
+      `- Explorers:`,
+      textStartX,
+      textStartY + fontSizePixels * 3,
+      width - 2
+    );
+    ctx.fillText(
+      `- Towns:`,
+      textStartX,
+      textStartY + fontSizePixels * 4,
+      width - 2
+    );
+    ctx.fillText(
+      `- Cities:`,
+      textStartX,
+      textStartY + fontSizePixels * 5,
+      width - 2
+    );
+
+    ctx.fillText(
+      `Dahan: 0`,
+      textStartX,
+      textStartY + fontSizePixels * 6,
+      width - 2
+    );
+    ctx.fillText(
+      `Presence: 0`,
+      textStartX,
+      textStartY + fontSizePixels * 7,
+      width - 2
     );
   };
 
